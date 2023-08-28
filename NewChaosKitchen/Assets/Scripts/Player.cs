b4260726bda866167d8e6a7ee20c0e05441982d3
Adnan <adnanfatawi26@gmail.com>
2023-08-28 13:15:48 +0700

    Character Controller

---
+++ NewChaosKitchen/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1f;
+        }
+
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+        transform.position += moveDir * Time.deltaTime * moveSpeed;
+    }
+}
