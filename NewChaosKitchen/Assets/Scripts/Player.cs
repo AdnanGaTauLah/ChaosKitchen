700a21b5384beb9a0e8ccccbf5d097e147c69601
Adnan <adnanfatawi26@gmail.com>
2023-09-19 20:41:25 +0700

    Character Visual. Rotation
    adding rotation on character & visual

--- NewChaosKitchen/Assets/Scripts/Player.cs
+++ NewChaosKitchen/Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotateSpeed;
+
     /// <summary>
     /// Update is called every frame, if the MonoBehaviour is enabled.
     /// </summary>
@@ -33,5 +35,7 @@ public class Player : MonoBehaviour
 
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += moveDir * Time.deltaTime * moveSpeed;
+
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 }
